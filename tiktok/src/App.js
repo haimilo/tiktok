a8a097759f07ca0ab08d57a20ffa9e83c37ede48
haimilo <ngotuehai97@gmail.com>
2021-10-29 23:29:54 +0700

    Update App.js

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -27,3 +27,53 @@ function App() {
 }
 
 export default App;
+
+/*
+import React, { useState }  from 'react'
+
+function App() {
+  const [workItem, setWorkItem] = useState("")
+  const [works, setWorks] = useState(JSON.parse(localStorage.getItem('job')) ?? [])
+  const handleAdd = () => {
+    setWorks(prev => {
+      const newWorks = (workItem !== "") ? [...prev, workItem] : [...prev];
+      const jsonWorks = JSON.stringify(newWorks);
+      localStorage.setItem('job', jsonWorks);
+      return newWorks;
+    })
+    setWorkItem("")
+  }
+  const handleDelete = (i) => setWorks((prev) => {
+    const newWorksDelete =  prev.filter((item) => (item !== prev[i]));
+    const jsonWorksDelete = JSON.stringify(newWorksDelete);
+    localStorage.setItem('job', jsonWorksDelete);
+    return newWorksDelete;
+  })
+  return (
+    <div style={{padding: 30}}>
+        <input
+          onChange={(e)=>setWorkItem(e.target.value)}
+          value = {workItem}
+        />
+        <button onClick = {handleAdd}>Add</button>
+        <ul>
+          {works.map((work, index) => {
+            return (
+                <li key={index}>
+                  {work}
+                  <button
+                    style={{marginLeft: 10, borderRadius: 10}}
+                    onClick = {() => handleDelete(index)}>
+                    Delete
+                  </button>
+                </li>
+            )
+          })}
+        </ul>
+    </div>
+  )
+}
+
+export default App;
+
+*/
