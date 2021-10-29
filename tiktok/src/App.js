dde68a3e4925788cea08bef40b40f154025c8d3d
haimilo <ngotuehai97@gmail.com>
2021-10-29 23:58:18 +0700

    Update App.js

--- tiktok/src/App.js
+++ tiktok/src/App.js
@@ -1,96 +1,15 @@
 import { useState } from 'react'
+import Content from './Content'
 
 function App() {
-  const [jobs, setJobs] = useState([]);
-  const [item, setItem] = useState('');
-  const handlerSubmit = () => {
-    setJobs(prev => {
-      const newWorks = (item !== "") ? [...prev, item] : [...prev];
-      const jsonWorks = JSON.stringify(newWorks);
-      localStorage.setItem('job', jsonWorks);
-      return newWorks;
-    })
-    setItem('');
-  }
-  const handleDelete = (i) => setJobs((prev) => {
-    const newWorksDelete = prev.filter((item) => (item !== prev[i]));
-    const jsonWorksDelete = JSON.stringify(newWorksDelete);
-    localStorage.setItem('job', jsonWorksDelete);
-    return newWorksDelete;
-  })
+  const [show, setShow] = useState(false);
+
   return (
     <div style={{ padding: 30 }}>
-      <input
-        onChange={(e) => setItem(e.target.value)}
-        value={item}
-      />
-      <button onClick={handlerSubmit}>Add</button>
-      <ul>
-        {jobs.map((work, index) => {
-          return (
-            <li key={index}>
-              {work}
-              <button
-                style={{ marginLeft: 10, borderRadius: 10 }}
-                onClick={() => handleDelete(index)}>
-                Delete
-              </button>
-            </li>
-          )
-        })}
-      </ul>
+      <button onClick={() => setShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
   );
 }
 
 export default App;
-
-/*
-import React, { useState }  from 'react'
-
-function App() {
-  const [workItem, setWorkItem] = useState("")
-  const [works, setWorks] = useState(JSON.parse(localStorage.getItem('job')) ?? [])
-  const handleAdd = () => {
-    setWorks(prev => {
-      const newWorks = (workItem !== "") ? [...prev, workItem] : [...prev];
-      const jsonWorks = JSON.stringify(newWorks);
-      localStorage.setItem('job', jsonWorks);
-      return newWorks;
-    })
-    setWorkItem("")
-  }
-  const handleDelete = (i) => setWorks((prev) => {
-    const newWorksDelete =  prev.filter((item) => (item !== prev[i]));
-    const jsonWorksDelete = JSON.stringify(newWorksDelete);
-    localStorage.setItem('job', jsonWorksDelete);
-    return newWorksDelete;
-  })
-  return (
-    <div style={{padding: 30}}>
-        <input
-          onChange={(e)=>setWorkItem(e.target.value)}
-          value = {workItem}
-        />
-        <button onClick = {handleAdd}>Add</button>
-        <ul>
-          {works.map((work, index) => {
-            return (
-                <li key={index}>
-                  {work}
-                  <button
-                    style={{marginLeft: 10, borderRadius: 10}}
-                    onClick = {() => handleDelete(index)}>
-                    Delete
-                  </button>
-                </li>
-            )
-          })}
-        </ul>
-    </div>
-  )
-}
-
-export default App;
-
-*/
